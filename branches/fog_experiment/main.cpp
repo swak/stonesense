f257f18f7b8b58f916d705cfdbc49f6b8a9784fe
Japa.Mala.Illo <Japa.Mala.Illo@4d48de78-bd66-11de-9616-7b1d4728551e>
2009-12-05 14:22:24 +0000

    added font-loading capabilities

--- branches/fog_experiment/main.cpp
+++ branches/fog_experiment/main.cpp
@@ -143,7 +143,8 @@ int main(void)
   config.backb = 160;
   loadConfigFile();
   set_uformat(U_ASCII);
-  //font = load_txt_font("curses_640x300.txt", NULL, NULL);
+  if(load_bitmap_font("font.bmp", NULL, NULL))
+  font = load_bitmap_font("font.bmp", NULL, NULL);
   //set debug cursor
   debugCursor.x = config.segmentSize.x / 2;
   debugCursor.y = config.segmentSize.y / 2;
