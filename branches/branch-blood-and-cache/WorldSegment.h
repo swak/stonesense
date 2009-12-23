77797b4e238143b867e94824d9bd6e0575d93f7a
tom.benda <tom.benda@4d48de78-bd66-11de-9616-7b1d4728551e>
2009-12-23 08:12:59 +0000

    Added 2d shadow buffer which improves the accuracy of lighting effects.  For some reason code requires a segment update in the screenshot code. Need to investigate that.

--- branches/branch-blood-and-cache/WorldSegment.h
+++ branches/branch-blood-and-cache/WorldSegment.h
@@ -4,57 +4,58 @@
 
 class WorldSegment{
 private:
-  vector<Block*> blocks;
+	vector<Block*> blocks;
 public:
 	int x, y, z;
 	int sizex, sizey, sizez;
-  Crd3D regionSize;
-  Block** blocksAsPointerVolume;
-  BITMAP* level;
+	Crd3D regionSize;
+	Block** blocksAsPointerVolume;
+	BITMAP* level;
+	bool **ShadowMap;
 
 	WorldSegment(int x, int y, int z, int sizex, int sizey, int sizez){
 		this->x = x; 
-    this->y = y; 
-    this->z = z - sizez + 1;
+		this->y = y; 
+		this->z = z - sizez + 1;
 		this->sizex = sizex; this->sizey = sizey; this->sizez = sizez;
 		level = NULL;
-		
-    regionSize.x = regionSize.y = regionSize.z = 0;
-    
-    uint32_t memoryNeeded = sizex * sizey * sizez * sizeof(Block*);
-    blocksAsPointerVolume = (Block**) malloc( memoryNeeded );
-    memset(blocksAsPointerVolume, 0, memoryNeeded);
 
-	ShadowMap = (bool **)malloc( sizex * sizey * sizeof(bool) );
+		regionSize.x = regionSize.y = regionSize.z = 0;
+
+		uint32_t memoryNeeded = sizex * sizey * sizez * sizeof(Block*);
+		blocksAsPointerVolume = (Block**) malloc( memoryNeeded );
+		memset(blocksAsPointerVolume, 0, memoryNeeded);
+
+		ShadowMap = (bool **)malloc( (sizey + 1) * sizeof(bool *) );
+		for(int i = 0; i < sizey; i++ )
+			ShadowMap[i] = (bool *)malloc( (sizex + 1) * sizeof(bool) );
 	}
 
-  ~WorldSegment(){
-    uint32_t num = (uint32_t)blocks.size();
-    for(uint32_t i = 0; i < num; i++){
-      delete(blocks[i]);
-    }
-    blocks.clear();
-    if (level)
-    	destroy_bitmap(level);
+	~WorldSegment(){
+		uint32_t num = (uint32_t)blocks.size();
+		for(uint32_t i = 0; i < num; i++){
+			delete(blocks[i]);
+		}
+		blocks.clear();
+		if (level)
+			destroy_bitmap(level);
 
-	delete [] ShadowMap;
-  }
+		free ( ShadowMap );
+	}
 
-  void Dispose(void){
-    free(blocksAsPointerVolume);
-  }
+	void Dispose(void){
+		free(blocksAsPointerVolume);
+	}
 
-  uint32_t getNumBlocks(){
-    return (uint32_t)blocks.size();
-  }
+	uint32_t getNumBlocks(){
+		return (uint32_t)blocks.size();
+	}
 
-  Block* getBlock(int32_t x, int32_t y, int32_t z);
-  Block* getBlockLocal(uint32_t x, uint32_t y, uint32_t z);
-  Block* getBlockRelativeTo(uint32_t x, uint32_t y, uint32_t z,  dirRelative direction);
-  Block* getBlock(uint32_t index);
-  void addBlock(Block* b);
-  void drawAllBlocks(BITMAP* target);
-  bool CoordinateInsideSegment(uint32_t x, uint32_t y, uint32_t z);
-
-  bool **ShadowMap;
+	Block* getBlock(int32_t x, int32_t y, int32_t z);
+	Block* getBlockLocal(uint32_t x, uint32_t y, uint32_t z);
+	Block* getBlockRelativeTo(uint32_t x, uint32_t y, uint32_t z,  dirRelative direction);
+	Block* getBlock(uint32_t index);
+	void addBlock(Block* b);
+	void drawAllBlocks(BITMAP* target);
+	bool CoordinateInsideSegment(uint32_t x, uint32_t y, uint32_t z);
 };
