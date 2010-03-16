e1b349a1a7f1385885ac05aecb508d941226d58a
4b524953 <4b524953@4d48de78-bd66-11de-9616-7b1d4728551e>
2010-03-16 15:50:21 +0000

    A bunch of build stuff made it into svn

--- branches/exp_dfh012/dfhack/build/Copy of build-MinGW32-release.bat
+++
@@ -1,6 +0,0 @@
-set path=c:\Program Files\CMake 2.6\bin;c:\MinGW\bin;%path%
-mkdir build-real
-cd build-real
-cmake ..\.. -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE:string=Release
-mingw32-make 2> log.txt
-pause
